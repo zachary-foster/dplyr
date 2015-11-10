338f0f325e1de8a2d500b0de2a275dd6e393faee
Romain Francois <romain@r-enthusiasts.com>
2015-11-10 11:26:06 +0100

    cache the results of in_data for faster retrieval

--- inst/include/dplyr/GlobalSubstitute.h
+++ inst/include/dplyr/GlobalSubstitute.h
@@ -18,25 +18,28 @@ namespace dplyr {
 
   private:
     RObject call ;
-    CharacterVector names ;
+    CharacterVector variable_names ;
     Environment env ;
     Function match ;
+    dplyr_hash_map<SEXP, bool> indata_map ;
 
     // substitute global(foo) with value of foo in the env
-    SEXP substitute_global(SEXP obj) ;
+    SEXP substitute_global(SEXP obj, int depth) ;
 
     // substutute column(x)
-    SEXP substitute_column(SEXP obj) ;
+    SEXP substitute_column(SEXP obj, int depth) ;
 
     // substitute x$y
-    SEXP substitute_dollar(SEXP obj) ;
+    SEXP substitute_dollar(SEXP obj, int depth) ;
 
     // traverse a LANGSXP
-    SEXP traverse_call(SEXP obj) ;
+    SEXP traverse_call(SEXP obj, int depth) ;
 
-    SEXP traverse(SEXP obj) ;
+    SEXP traverse(SEXP obj, int depth) ;
 
-    bool in_data( Symbol s) ;
+    bool in_data( SEXP s) ;
+
+    void cache_indata_map() ;
 
   } ;
 
--- src/hybrid.cpp
+++ src/hybrid.cpp
@@ -3,70 +3,119 @@
 using namespace Rcpp ;
 using namespace dplyr ;
 
+#define DBG_LEVEL 1
+
+#if DBG_LEVEL > 0
+  #define MSG(msg) for(int i=0; i<depth; i++) Rprintf( "  " ) ; Rprintf(msg) ;
+  #define DBG(msg, obj) MSG(msg); Rprintf(" [%s] : ", type2name(obj) ) ; Rf_PrintValue(obj)
+#else
+  #define MSG(msg)
+  #define DBG(msg,obj)
+#endif
+
 namespace dplyr {
 
   GlobalSubstitute::GlobalSubstitute( SEXP call_, const DataFrame& data, const Environment& env_ ) :
     call(call_),
-    names(data.names()),
+    variable_names(data.names()),
     env(env_),
     match("match")
   {
-    call = traverse(call);
+    cache_indata_map() ;
+    call = traverse(call, 0);
   }
 
-  bool GlobalSubstitute::in_data( Symbol s){
-    return as<int>(match(CharacterVector::create(PRINTNAME(s)), names)) != NA_INTEGER ;
+  void GlobalSubstitute::cache_indata_map(){
+    int n = variable_names.size() ;
+    for(int i=0; i<n; i++){
+      indata_map[ Rf_install(variable_names[i]) ] = true ;
+    }
   }
 
-  SEXP GlobalSubstitute::traverse(SEXP obj){
+  bool GlobalSubstitute::in_data( SEXP s){
+    bool res = false ;
+    dplyr_hash_map<SEXP,bool>::const_iterator it = indata_map.find(s) ;
+    Rprintf( "in_data( %s ) = ", CHAR(PRINTNAME(s))) ;
+    if( it == indata_map.end() ){
+        Rprintf(" (new  ) ") ;
+        indata_map[s] = res = as<int>(match(CharacterVector::create(PRINTNAME(s)), variable_names)) != NA_INTEGER ;
+    } else {
+        Rprintf(" (cached) ") ;
+        res = it->second ;
+    }
+
+    Rprintf("%s\n", res ? "true" : "false") ;
+    return res ;
+  }
+
+  SEXP GlobalSubstitute::traverse(SEXP obj, int depth){
+    DBG("traverse. obj", obj) ;
+
     if(TYPEOF(obj) == LANGSXP){
-      return traverse_call(obj) ;
+      return traverse_call(obj, depth + 1) ;
     }
     return obj ;
   }
 
-  SEXP GlobalSubstitute::substitute_dollar(SEXP obj){
-    SEXP lhs = CADR(obj) ;
-    SEXP rhs = CADDR(obj) ;
-
+  SEXP GlobalSubstitute::substitute_dollar(SEXP obj, int depth){
+    RObject lhs = CADR(obj) ;
+    RObject rhs = CADDR(obj) ;
+    DBG( "substitute_dollar", obj ) ;
     if( TYPEOF(lhs) == LANGSXP ){
-      SETCAR(CDR(obj), traverse_call(lhs)) ;
+      lhs = traverse_call(lhs, depth+1) ;
     }
+    if( TYPEOF(lhs) == SYMSXP ){
+      if( TYPEOF(rhs) == SYMSXP ){
+        if( !in_data(lhs) ){
+          Language expr = Rf_lang3( R_DollarSymbol, lhs, rhs) ;
+          return expr.eval(env) ;
+        }
+      }
+    } else {
+      Language expr = Rf_lang3( R_DollarSymbol, lhs, rhs) ;
+      return expr.eval(env);
+    }
+    Rf_PrintValue(obj) ;
     return obj ;
   }
 
-  SEXP GlobalSubstitute::traverse_call(SEXP obj){
+  SEXP GlobalSubstitute::traverse_call(SEXP obj, int depth){
+    DBG("traverse_call", obj) ;
     if( CAR(obj) == Rf_install("global") ){
-      return substitute_global(obj) ;
+      return substitute_global(obj, depth+1) ;
     }
 
     if( CAR(obj) == Rf_install("column")){
-      return substitute_column(obj) ;
+      return substitute_column(obj, depth+1) ;
     }
 
     if( CAR(obj) == Rf_install("order_by") ){
       return obj ;
     }
 
-    // if( CAR(obj) == R_DollarSymbol && TYPEOF(CADDR(obj)) == SYMSXP ){
-    //   return substitute_dollar(obj) ;
-    // }
+    if( CAR(obj) == R_DollarSymbol ){
+      RObject out = substitute_dollar(obj, depth + 1)  ;
+      MSG("length(out)="); Rprintf("%d, [%s]", Rf_length(out), type2name(out)) ;
+      MSG( "substitute_dollar.out") ;
+      Rf_PrintValue(out) ;
+      return out ;
+    }
 
     if( TYPEOF(CAR(obj)) == LANGSXP ){
-      SETCAR(obj, traverse(CAR(obj))) ;
+      SETCAR(obj, traverse(CAR(obj), depth + 1)) ;
     }
 
     SEXP p = obj ;
     if( TYPEOF(CAR(p)) == SYMSXP ) p = CDR(p) ;
     while( !Rf_isNull(p) ){
-      SETCAR(p, traverse(CAR(p))) ;
+      SETCAR(p, traverse(CAR(p), depth + 1)) ;
       p = CDR(p) ;
     }
 
     return obj ;
   }
 
-  SEXP GlobalSubstitute::substitute_global( SEXP obj ){
+  SEXP GlobalSubstitute::substitute_global( SEXP obj, int depth ){
     if(Rf_length(obj) != 2 || TYPEOF(CADR(obj)) != SYMSXP)
       stop("global only handles symbols") ;
     SEXP symb = CADR(obj) ;
@@ -74,7 +123,7 @@ namespace dplyr {
     return res ;
   }
 
-  SEXP GlobalSubstitute::substitute_column( SEXP obj ){
+  SEXP GlobalSubstitute::substitute_column( SEXP obj, int depth ){
     if( Rf_length(obj) != 2)
       stop("unsupported form for column") ;
 
